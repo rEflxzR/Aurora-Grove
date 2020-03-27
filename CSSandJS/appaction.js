650c109bf3ab8aacf669c75edf6c2c65d917bb74
PRIYANSHU SINGH <anshu.singh1429@gmail.com>
2020-03-27 20:30:32 +0530

    Final Model with JPG Photos

---
+++ CSSandJS/appaction.js
@@ -0,0 +1,29 @@
+$("#exp").on('click',function() {
+    $('html, body').animate({
+        'scrollTop' : $("#con-3").position().top
+    });
+});
+
+$(".faq").on('click',function() {
+    $('html, body').animate({
+        'scrollTop' : $("#con-1").position().top
+    });
+});
+
+$(".expl").on('click',function() {
+    $('html, body').animate({
+        'scrollTop' : $("#gall").position().top
+    });
+});
+
+$("#tmt1").on('click',function() {
+    $('html, body').animate({
+        'scrollTop' : $("#con-3").position().top
+    });
+});
+
+$("#tmt2").on('click',function() {
+    $('html, body').animate({
+        'scrollTop' : $("#gall").position().top
+    });
+});
